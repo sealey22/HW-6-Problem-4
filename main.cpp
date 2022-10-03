8cb09036d7d5d5fdc15d11480e5f0a00aba973d6
Hsealey <112498796+sealey22@users.noreply.github.com>
2022-10-03 02:41:59 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+int Month(char);
+int main() {
+  int T;// number  of harry potters classmates
+  cout << " Enter the number of classmates: ";
+  cin >> T;
+
+  
+  int myMonth = Month(int month);
+
+  
+
+    
+    
+
+    
+  
+
+  
+    }
+int Month(int m){
+  char month;
+  cout << "Enter month: ";
+  cin >> month;
+  char Jan = 1;
+  char Feb = 2;
+  char Mar = 3;
+  char Apr = 4;
+  char May = 5;
+  char Jun = 6;
+  char Jul = 7;
+  char Aug = 8;
+  char Sep = 9;
+  char Oct = 10;
+  char Nov = 11;
+  char Dec = 12;
+  
+      
+      
+      
+    
+    
+
+
+    
+
+
+
+
+
+  return month;
+}
